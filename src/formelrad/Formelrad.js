5b0dc668d38aa8e4ebe1dd141e434df4cc096f41
mfedorchenko <mfedorchenko@ethz.ch>
2025-05-26 15:13:35 +0200

    Values defines color of message text.

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -45,28 +45,28 @@ export default function Formelrad() {
             setValues(values => ({ ...values, message: "" }));
             if (values.u === "" && values.i === "") {
                 /*calculate u and i */
-                setValues(values => ({ ...values, u: Math.sqrt(values.p * values.r) }));
-                setValues(values => ({ ...values, i: Math.sqrt(values.p / values.r) }));
+                setValues(values => ({...values, u: Math.sqrt(values.p * values.r)}));
+                setValues(values => ({...values, i: Math.sqrt(values.p / values.r)}));
             } else if (values.u === "" && values.r === "") {
                 /*calculate u and r */
-                setValues(values => ({ ...values, u: values.p / values.i }));
-                setValues(values => ({ ...values, r: values.p / values.i / values.i }));
+                setValues(values => ({...values, u: values.p / values.i}));
+                setValues(values => ({...values, r: values.p / values.i / values.i}));
             } else if (values.u === "" && values.p === "") {
                 /*calculate u and p */
-                setValues(values => ({ ...values, u: values.i * values.r }));
-                setValues(values => ({ ...values, p: values.i * values.i * values.r }));
+                setValues(values => ({...values, u: values.i * values.r}));
+                setValues(values => ({...values, p: values.i * values.i * values.r}));
             } else if (values.i === "" && values.r === "") {
                 /*calculate i and r */
-                setValues(values => ({ ...values, i: values.p / values.u }));
-                setValues(values => ({ ...values, r: values.u * values.u / values.p }));
+                setValues(values => ({...values, i: values.p / values.u}));
+                setValues(values => ({...values, r: values.u * values.u / values.p}));
             } else if (values.i === "" && values.p === "") {
                 /*calculate i and p */
-                setValues(values => ({ ...values, i: values.u / values.r }));
-                setValues(values => ({ ...values, p: values.u * values.u / values.r }));
+                setValues(values => ({...values, i: values.u / values.r}));
+                setValues(values => ({...values, p: values.u * values.u / values.r}));
             } else {
                 /*calculate r and p */
-                setValues(values => ({ ...values, r: values.u / values.i }));
-                setValues(values => ({ ...values, p: values.u * values.i }));
+                setValues(values => ({...values, r: values.u / values.i}));
+                setValues(values => ({...values, p: values.u * values.i}));
             }
         }
     }
@@ -85,10 +85,10 @@ export default function Formelrad() {
                     <InputField color={colors.r} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <InputField color={colors.p} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
-                    <p>{values.message}</p>
+                    <p style={{ color: colors.message }}>{values.message}</p>
                     <button style={{margin:10}} onClick={handleClear}>Clear</button>
-                </form>
-            </section>
-        </>
+                 </form>
+             </section>
+         </>
     )
 }
